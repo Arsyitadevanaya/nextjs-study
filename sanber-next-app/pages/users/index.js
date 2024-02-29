611a14ffe55749460d336895638b081c7b5f538a
Arsyita20 <arsyitadevanaya@gmail.com>
2024-02-29 18:41:54 +0700

    Api Routes

--- sanber-next-app/pages/users/index.js
+++ sanber-next-app/pages/users/index.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Layout from '@/layout'
 
 export default function Users() {
+  useEffect(()=>{
+    fetch("/api/hello")
+    .then(res=>res.json())
+    .then((res)=>console.log("response=>", res))
+    .catch((err)=>console.log("err =>",err))
+  })
   return (
     <Layout metaTitle="users">
       <p>Users</p>
